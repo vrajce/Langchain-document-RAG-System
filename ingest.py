df00a1e10342b25164ff941595c2028062a1d48c
vrajce <parmarvraj884@gmail.com>
2026-03-03 23:23:32 +0530

    chunking of data completed

--- ingest.py
+++ ingest.py
@@ -1,21 +1,65 @@
-from langchain_community.document_loaders import DirectoryLoader , TextLoader
-from langchain_text_splitters import MarkdownHeaderTextSplitter
+from typing import Any, final
+from langchain_community.document_loaders import DirectoryLoader , TextLoader , UnstructuredMarkdownLoader
+from langchain_community.document_loaders.base_o365 import CHUNK_SIZE
+from langchain_text_splitters import MarkdownHeaderTextSplitter , RecursiveCharacterTextSplitter
 
-loader = DirectoryLoader('./docs' , glob="**/*.mdx" ,loader_cls=TextLoader, show_progress=True , loader_kwargs={'encoding':'utf-8'})
-raw_docs = loader.load()
 
-print(f'length of raw docs: {len(raw_docs)}')
+loader = DirectoryLoader(
+    './docs' , 
+    glob="**/*.mdx" ,
+    loader_cls=UnstructuredMarkdownLoader, 
+    show_progress=True , 
+    loader_kwargs={'encoding':'utf-8'}
+)
 
-# print(raw_docs[5])
+# print(loader)
 headers_to_split_on=[("#", "Header 1"),("##", "Header 2"),("###", "Header 3")]
 markDownSplitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
 
 md_chunks=[]
 
-for doc in raw_docs:
-    splits = markDownSplitter.split_text(doc)
-    print(f)
+for doc in loader.lazy_load():
+    
+    splits = markDownSplitter.split_text(doc.page_content)
+    
     for split in splits:
+        split.metadata.update(doc.metadata)
+        source = split.metadata['source'].lower()
+        print(f"\n\n\n\n\n\n\n\n\n\n\n\n\n {source}")
+        
+        # Check specific libraries FIRST
+        if 'langsmith' in source:
+            ecosystem = 'langsmith'
+        elif 'langgraph' in source:
+            ecosystem = 'langgraph'
+        elif 'integrations' in source:
+            ecosystem = 'integrations'
+        elif 'concepts' in source:
+            ecosystem = 'concepts'
+        elif 'javascript' in source:
+            ecosystem = 'javascript'
+        elif 'python' in source:
+            ecosystem = 'langchain'  # The 'python' folder is usually the main LangChain SDK
+        elif 'contributing' in source:
+            ecosystem = 'contributing'
+        else:
+            ecosystem = 'general'
+        
+        split.metadata['ecosystem'] = ecosystem
         md_chunks.append(split)
+        print(f"\n\n\n\n\n\n\n : split :  {split}")
+
     
-    
+
+secondarySplitter = RecursiveCharacterTextSplitter(
+    chunk_size=1200,
+    chunk_overlap=200,
+)
+
+final_splitted = secondarySplitter.split_documents(md_chunks)
+print(f"✅ Final Production Chunks: {len(final_splitted)}")
+
+
+with open('final_chunks.txt' , 'w' , encoding='UTF-8') as f:
+    for doc in final_splitted:
+        f.write(doc.page_content + '\n\n')
